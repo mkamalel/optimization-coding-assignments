c200574525517b2c7c90fb7c715b250b4370f662
Mahmoud Kamaleldin <amahmoud98@gmail.com>
2021-03-20 15:02:11 -0700

    Adding constrained pso implementation with exterior penalty scheme

--- pso/pso.py
+++ pso/pso.py
@@ -3,9 +3,23 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from matplotlib import animation
 from statistics import mean 
+import time
+import seaborn as sns
+import sys
+
+sns.set(rc={'figure.figsize':(10, 6)})
+
 
 func1 = lambda x : x[0]**2 + x[1]**2 - x[0]*x[1] - 4*x[0]- x[1]
 func2 = lambda x : (1-x[0])**2 + (-(x[0]**2)+x[1])**2
+func3_unc = lambda x : (1-x[0])**2 + (-(x[0]**2)+x[1])**2
+func3_constraint = lambda x : -10*x[0]-3*x[1]+25
+
+def func3(x, r):
+  if -10*x[0]-3*x[1]+25 >= 0:
+    return (1-x[0])**2 + (-(x[0]**2)+x[1])**2 + r*(-10*x[0]-3*x[1]+25)**2
+  else:
+    return (1-x[0])**2 + (-(x[0]**2)+x[1])**2
 
 class Particle():
   def __init__(self, x_vec, velocity_vec, max_velocity, func):
@@ -40,26 +54,42 @@ class Particle():
 
 
 
-def PSO(func, num_vars, x_limits=[-100.0,100.0], velocity_limits=[-100,100], num_particles=30,
-        max_velocity=100, w=0.7, c1=2, c2=2, tol=1e-6, max_iter=400, stopping_criteria='Function Value',
-        stall_iterations_limit=10):
+def PSO(func, num_vars, x_limits=[-100.0,100.0], velocity_limits=[-1000,1000], num_particles=30,
+        max_velocity=1000, w=0.7, c1=2, c2=2, tol=1e-6, max_iter=400, stopping_criteria='Function Value',
+        stall_iterations_limit=10, verbose=False):
   """
-  Perform a BFGS optimization of a function in the form of a Sympy expression.
+  Perform a PSO optimization of a function.
 
   Parameters
   ----------
   func : Function handle
       The objective function.
-  g_func_vec : List of function handles
-      Gradient vector.
-  point_vec : column array
-      The initial starting points for minimum.
-  tolerance : float.
-      Stopping criteria for optimum.
-  maxIter : int.
+  num_vars: int
+      Number of variables in cost function
+  x_limits: list
+      Upper and lower bounds of x vector
+  velocity_limits: list
+      Upper and lower bounds of velocity
+  num_particles: int
+      Number of particles in population
+  max_velocity: number
+      Max velocity of each particle
+  w: float
+      Inertia factor
+  c1: float
+      Learning factor 1
+  c2: float
+      Learning factor 2
+  tol : float
+      Stopping criteria evaluation for optimum.
+  max_iter : int
       Max number of iterations.
+  stopping_criteria: string
+      Stopping criteria, either Function Value or Velocity
+  stall_iterations_limit: int
+      Max stall iterations
   verbose : bool.
-      If set to True, print out point, function value, and alpha every iteration.
+      If set to True, print out point, function value every iteration.
   """
 
   x_init_vectors = np.random.uniform(low=x_limits[0], high=x_limits[1], size=(num_particles, num_vars))
@@ -76,6 +106,7 @@ def PSO(func, num_vars, x_limits=[-100.0,100.0], velocity_limits=[-100,100], num
   g_best = None
   g_func_best = None
   g_func_prev_best = 0
+  initial_time = time.time()
   while iterations < max_iter:
     for particle in population:
       func_val = particle.calculate_func_val()
@@ -85,10 +116,10 @@ def PSO(func, num_vars, x_limits=[-100.0,100.0], velocity_limits=[-100,100], num
           g_best = particle.x_vec
           g_func_prev_best = g_func_best
           g_func_best = func_val
-          print(f"stall_iterations:{stall_iterations}|g_best:{g_best}|g_func_best:{g_func_best}")
+          if verbose == True:
+            print(f"stall_iterations:{stall_iterations}|g_best:{g_best}|g_func_best:{g_func_best}")
           stall_iterations = 0
       except Exception as e:
-          print(e)
           g_best = particle.x_vec
           g_func_best = func_val
           g_func_prev_best = g_func_best+10
@@ -98,12 +129,14 @@ def PSO(func, num_vars, x_limits=[-100.0,100.0], velocity_limits=[-100,100], num
 
     if stopping_criteria == "Velocity":
       if mean([np.linalg.norm(particle.velocity_vec) for particle in population]) < tol and stall_iterations > stall_iterations_limit:
+        total_time = time.time() - initial_time
         print(f"Optimum Found after {iterations} iterations at x={g_best}, f={g_func_best}")
-        return population, g_best_list, g_func_best_list, iterations
+        return population, g_best_list, g_func_best_list, iterations, total_time
     else:
       if abs(g_func_best-g_func_prev_best) < tol and stall_iterations > stall_iterations_limit:
+        total_time = time.time() - initial_time
         print(f"Optimum Found after {iterations} iterations at x={g_best}, f={g_func_best}")
-        return population, g_best_list, g_func_best_list, iterations
+        return population, g_best_list, g_func_best_list, iterations, total_time
 
     for particle in population:
       particle.update(g_best, c1, c2, w)
@@ -111,24 +144,99 @@ def PSO(func, num_vars, x_limits=[-100.0,100.0], velocity_limits=[-100,100], num
     iterations+=1
     stall_iterations+=1
 
-  print(f"Optimum Found after {iterations} iterations at x={g_best}, f={g_func_best}")
-  return population, g_best_list, g_func_best_list, iterations
+  total_time = time.time() - initial_time
+  #print(f"Optimum Found after {iterations} iterations at x={g_best}, f={g_func_best}")
+  return population, g_best_list, g_func_best_list, iterations, total_time
+
+def penaltyPSO(func, num_vars, constraint_func, x_limits=[-100.0,100.0], velocity_limits=[-1000,1000], num_particles=30,
+        max_velocity=1000, w=0.7, c1=2, c2=2, tol=1e-6, max_iter=400, stopping_criteria='Function Value',
+        stall_iterations_limit=10, penalty_iter=500):
+  """
+  Perform a PSO optimization of a function.
+
+  Parameters
+  ----------
+  func : Function handle
+      The objective function.
+  num_vars: int
+      Number of variables in cost function
+  constraint_func: Function handle
+      Constraint function
+  x_limits: list
+      Upper and lower bounds of x vector
+  velocity_limits: list
+      Upper and lower bounds of velocity
+  num_particles: int
+      Number of particles in population
+  max_velocity: number
+      Max velocity of each particle
+  w: float
+      Inertia factor
+  c1: float
+      Learning factor 1
+  c2: float
+      Learning factor 2
+  tol : float
+      Stopping criteria evaluation for optimum.
+  max_iter : int
+      Max number of iterations.
+  stopping_criteria: string
+      Stopping criteria, either Function Value or Velocity
+  stall_iterations_limit: int
+      Max stall iterations
+  penalty_iter: int
+      Max evaluations of PSO
+  """
+  r_iterations = 0
+  r = 0
+  prev_pso_best = 1e20
+  pso_best = [1e99]
+  pso_x_best = None
+  pso_pop_best = None
+  iterations_best = None
+  while r_iterations < penalty_iter:
+    input_func = lambda x : func(x, r)
+    pop, x_best, pso_val, iterations, _ = PSO(input_func, num_vars, tol=tol, stopping_criteria=stopping_criteria)
+
+    if pso_val[-1] < pso_best[-1] and constraint_func(x_best[-1]) <= 0:
+      pso_x_best = x_best
+      pso_pop_best = pop
+      iterations_best = iterations
+      prev_pso_best = pso_best[-1]
+      pso_best = pso_val
+      r_best = r
+      print(f"Penalty: x_best:{x_best[-1]}|func_best:{pso_best[-1]}|r:{r}")
+
+    if abs(pso_best[-1]-prev_pso_best) < tol:
+      print(f"Penalty Optimum Found after {r_iterations} iterations at x={pso_x_best[-1]}, f={pso_best[-1]}, r:{r_best}, constraint={constraint_func(pso_x_best[-1])}")
+      return pso_pop_best, pso_x_best, pso_best, iterations_best, input_func,r_best
+    else:
+     print(f"Penalty: x_best:{x_best[-1]}|func_best:{pso_best[-1]}|r:{r}|constraint:{constraint_func(x_best[-1])}")
+
+    r+=1 
+    r_iterations+=1
+
+  print(f"Penalty Optimum Found after {r_iterations} iterations at x={pso_x_best[-1]}, f={pso_best[-1]}, r:{r_best}, constraint={constraint_func(pso_x_best[-1])}")
+  return pso_pop_best, pso_x_best, pso_best, iterations_best, input_func,r_best
+
+
 
 def visualizeHistory2D(func=None, data=None, bounds=None, 
                        minima=None, g_best=None, g_func_best=None):
     """Visualize the process of optimizing
     # Arguments
         func: object function
-        history: dict, object returned from pso above
+        data: list, population of particles returned from PSO function
         bounds: list, bounds of each dimention
         minima: list, the exact minima to show in the plot
-        func_name: str, the name of the object function
-        save2mp4: bool, whether to save as mp4 or not
+        g_best: list, global x best at each iteration
+        g_func_best: list, global func value best at each iteration
     """
-
     print('## Visualizing optimizing')
     assert len(bounds)==2
 
+    plot_list = [[list(particle.x_vec_history[j]) for particle in data] for j in range(0, iterations)]
+    data = [[list(particle.x_vec_history[j]) for particle in data] for j in range(0, iterations)]
     # define meshgrid according to given boundaries
     x = np.linspace(bounds[0], bounds[1], 200)
     y = np.linspace(bounds[0], bounds[1], 200)
@@ -154,11 +262,6 @@ def visualizeHistory2D(func=None, data=None, bounds=None,
         ax2.set_title(f'g_func_best={g_func_best[frame]}')
         ax2.set_xlim(2,len(g_func_best))
         ax2.set_ylim(g_func_best[-1]*1.1,int(mean(g_func_best[0:10])*1.1))
-        #ax2.set_yscale('log')
-
-        # data to be plot
-        #data = history['particles'][frame]
-        #global_best = np.array(history['global_best_fitness'])
 
         # contour and global minimum
         contour = ax1.contour(X,Y,Z, levels=50, cmap="magma")
@@ -184,17 +287,135 @@ def visualizeHistory2D(func=None, data=None, bounds=None,
 
 
 
-
-    ani.save('PSO_population.gif', writer="imagemagick")
+    # Uncomment to save as gif
+    #ani.save('PSO_population.gif', writer="imagemagick")
 
     plt.show()
 
+def plot_bar(x, y, xlabel, ylabel, title):
+  plt.figure()
+  plt.bar(x, y)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.title(title)
+  for i, v in enumerate(y):
+    plt.text(i - 0.1, v + 0.001, str(round(v,3)))
+
+  plt.savefig(f'func2/{title}.png', bbox_inches='tight')
+
 if __name__ == '__main__':
+  # Function Select
+  #"""
+  func3Flag = False
+  try:
+    if sys.argv[1] == "3":
+      eval_func = func2
+      funcNum = 3
+      func3Flag = True
+    elif sys.argv[1] == "2":
+      eval_func = func2
+      funcNum = 2
+    else:
+      eval_func = func1
+      funcNum = 1
+  except:
+    eval_func = func1
+    funcNum = 1
 
-  pop, x_best, func_best, iterations = PSO(func2, 2, tol=1e-6)
 
-  plot_list = [[list(particle.x_vec_history[j]) for particle in pop] for j in range(0, iterations)]
+  if func3Flag == False:
+    # Run PSO 5 times
+    func_best_list = []
+    iterations_list = []
+    runtime_list = []
+    for i in range(0,5):
+      pop, x_best, func_best, iterations, runtime = PSO(eval_func, 2, tol=1e-6, stopping_criteria="Function Value")
+      func_best_list.append(func_best[-1])
+      iterations_list.append(iterations)
+      runtime_list.append(runtime)
 
-  visualizeHistory2D(func=func2, data=plot_list, bounds=[-100,100], minima=x_best[-1], g_best=x_best, g_func_best=func_best)
-  #plt.scatter(*zip(*plot_list[0]))
-  #plt.show()
+    # Function Value
+    plot_bar(range(len(func_best_list)), func_best_list, "Function Value", "Run", f"Function Value Best for Function {funcNum} over 5 Runs")
+
+    # Num of Iterations
+    #plt.figure()
+    #plt.xlabel("Iterations")
+    #plt.ylabel("Frequency")
+    #plt.title(f"Function {funcNum} iterations disribution over 5000 runs")
+    #plt.hist(iterations_list, bins=100)
+    #plot_bar(range(len(iterations_list)), iterations_list, "# of Iterations", "Run", f"# of Iterations to Find Optimum for Function {funcNum} over 5 Runs")
+
+    # Runtime
+    plot_bar(range(len(runtime_list)), runtime_list, "Runtime(s)", "Run", f"Runtime to Find Optimum for Function {funcNum} over 5 Runs")
+
+    # Change # of particles [10, 100, 1000]
+    func_best_list = []
+    iterations_list = []
+    runtime_list = []
+    popSize = ["10", "100", "1000"]
+    for num in popSize:
+      pop, x_best, func_best, iterations, runtime = PSO(eval_func, 2, tol=1e-6, stopping_criteria="Function Value", num_particles=int(num))
+      func_best_list.append(func_best[-1])
+      iterations_list.append(iterations)
+      runtime_list.append(runtime)
+
+    # Function Value
+    plot_bar(popSize, func_best_list, "Function Value", "Run", f"Function Value Best for Function {funcNum} with Varying Population Sizes")
+
+    # Num of Iterations
+    plot_bar(popSize, iterations_list, "# of Iterations", "Run", f"# of Iterations to Find Optimum for Function {funcNum} with Varying Population Sizes")
+
+    # Runtime
+    plot_bar(popSize, runtime_list, "Runtime(s)", "Run", f"Runtime to Find Optimum for Function {funcNum} with Varying Population Sizes")    
+
+    # Change c1,c2 [0.5, 5]
+    func_best_list = []
+    iterations_list = []
+    runtime_list = []
+    learning_factors = ["0.5", "5"]
+    for c in learning_factors:
+      pop, x_best, func_best, iterations, runtime = PSO(eval_func, 2, tol=1e-6, stopping_criteria="Function Value", c1=float(c), c2=float(c))
+      func_best_list.append(func_best[-1])
+      iterations_list.append(iterations)
+      runtime_list.append(runtime)
+
+    # Function Value
+    plot_bar(learning_factors, func_best_list, "Function Value", "Run", f"Function Value Best for Function {funcNum} with Varying Learning Factors")
+
+    # Num of Iterations
+    plot_bar(learning_factors, iterations_list, "# of Iterations", "Run", f"# of Iterations to Find Optimum for Function {funcNum} with Varying Learning Factors")
+
+    # Runtime
+    plot_bar(learning_factors, runtime_list, "Runtime(s)", "Run", f"Runtime to Find Optimum for Function {funcNum} with Varying Learning Factors")    
+    #"""
+  else:
+    #Function 3
+    r_best_list = []
+    func_best_list = []
+    constraint_list = []
+    iterations_list = []
+    for i in range(0,5):
+      pop, x_best, func_best, iterations, penalty_func, r_best = penaltyPSO(func3, 2, func3_constraint, tol=1e-6, stopping_criteria="Function Value")
+      func_best_list.append(func_best[-1])
+      iterations_list.append(iterations)
+      r_best_list.append(r_best)
+      constraint_list.append(func3_constraint(x_best[-1]))
+
+    # Function Value
+    plot_bar(range(len(func_best_list)), func_best_list, "Function Value", "Run", f"Constrained Optimum for Function {funcNum} over 5 Runs")
+
+    # Num of Iterations
+    plot_bar(range(len(func_best_list)), iterations_list, "# of Iterations", "Run", f"# of Iterations to Find Constrained Optimum for Function {funcNum} over 5 Runs")
+
+    # Runtime
+    plot_bar(range(len(func_best_list)), r_best_list, "Penalty Factor", "Run", f"Penalty Factor to Find Constrained Optimum for Function {funcNum} over 5 Runs")    
+    #"""
+
+    # Constraint Value
+    plot_bar(range(len(func_best_list)), constraint_list, "Constraint Value", "Run", f"Constraint Value for Function {funcNum} over 5 Runs")
+
+
+  # Uncomment to see PSO visualization
+  #visualizeHistory2D(func=eval_func, data=pop, bounds=[-100,100], minima=x_best[-1], g_best=x_best, g_func_best=func_best)
+
+  plt.show()
